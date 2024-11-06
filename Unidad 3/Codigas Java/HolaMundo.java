70a5b1f03607b2289ce2f29be4f19af7c69d4969
Ramon <ramonernesto.contreras@alumnos.ulagos.com>
2024-11-05 23:43:53 -0300

    java1

---
+++ Unidad 3/Codigas Java/HolaMundo.java
@@ -0,0 +1,14 @@
+
+package holamundo;
+
+/**
+ *
+ * @author ramon
+ */
+public class HolaMundo {
+
+    public static void main(String[] args) {
+        System.out.println("HolaMundo!");
+    }
+    
+}
