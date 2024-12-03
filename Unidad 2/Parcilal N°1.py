7e7125c8a1ce7ca4d22230956dcdd7409dc99358
Ramon <ramonernesto.contreras@alumnos.ulagos.com>
2024-12-03 02:18:36 -0300

    upgrade codigo

--- Unidad 2/Parcilal N°1.py
+++ Unidad 2/Parcilal N°1.py
@@ -1,33 +1,40 @@
 class Gato:
-    def __init__(self, nombre, edad, color,energia, hambre, ubicación):
-        self.nombre = nombre
-        self.edad = edad
-        self.color = color
-        self.energia = energia
-        self.hambre = hambre
-        self. ubicación= ubicación
+    def __init__(self, nombre, edad, color, ubicación):
+        self.__nombre = nombre
+        self.__edad = edad
+        self.__color = color
+        self.__energia = 100
+        self.__hambre = 0
 
-    def jugar(self, energia):
-        if energia > 20:
+    def get_nombre(self):
+        return self.__nombre
+
+    def __str__(self):
+        return (f"Nombre: {self.__nombre}, Edad: {self.__edad}, Colo: {self.__color}"
+                f"Energía: {self.__energia}, Hambre: {self.__hambre}")
+
+    def jugar(self):
+        if self.__energia > 20:
             print ("El gato tiene mucha energia y quiere jugar")
+            self.__energia -= 20
+            self.__hambre += 30
         else:
             print("El gato esta muy cansado para jugar")
     
     def comer(self, hambre):
-        if hambre > 75:
+        if hambre > 50:
             print ("El gato esta hambriento, le diste de comer")
-            
+            self.__hambre -= 40
+            self.__energia += 20
         else:
             print("El gato no tiene hambre")
 
     
 class Area:
-    def __init__(self, nombrearea, capacidadclientes, cantidadgatosporarea, cantidadgatostotal, capacidadgatos):
-        self.nombre = nombrearea
-        self.gatos = cantidadgatosporarea
-        self.gatostotal = cantidadgatostotal
-        self.capacidadclientes = capacidadclientes
-        self.capacidadgatos = capacidadgatos
+    def __init__(self, nombre, capacidad, cantidadgatos):
+        self.nombre = nombre
+        self.gatos = cantidadgatos
+        self.capacidadclientes = capacidad
 
     def añadirgato(self, capacidadgatos, cantidadgatosporarea):
         if cantidadgatosporarea < capacidadgatos:
@@ -43,8 +50,8 @@ class Inventario:
         self.juguetes = juguetegatos
 
 
-gato =(Gato, 1, 3, "naranjo", 100, 0, "Primer piso")
-gato2 =(Gato, 2, 6, "negro", 80, 15, "Terraza")
+gato =(Gato, 1, 3, "naranjo")
+gato2 =(Gato, 2, 6, "negro")
 print(gato)
 Area1 = (Area, "Primer piso",  4, 4, 12, 6)
 print(Area1)
